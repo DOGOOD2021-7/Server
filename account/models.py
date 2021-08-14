3f1fe29641e1a4901a4cb349a5f7f6afdb58bf57
suy2on <tnwjdrms@naver.com>
2021-08-14 10:47:54 +0900

    Feat: add Gym model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Gym(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gym_name = models.CharField(max_length=100)
+    address = models.TextField()
+    logo = models.ImageField()
+    website = models.URLField()
+    profile1 = models.ImageField()
+    profile2 = models.ImageField()
+    profile3 = models.ImageField()
+
+
+
+
+
+
