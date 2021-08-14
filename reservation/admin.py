02edef7708af2a48bbabe7a13c028f64075e4754
suy2on <tnwjdrms@naver.com>
2021-08-14 20:36:49 +0900

    Feat: add reservation admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id','state','reason','','reserved_date','phone_num','client_name','gym','dieter')
+    list_display_links = ('id','state','reason','','reserved_date','phone_num','client_name','gym','dieter')
