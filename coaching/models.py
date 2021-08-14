12babfed2075c097429f86528bcc39fbfc944bc8
HoYoungChun <ho723@naver.com>
2021-08-14 20:37:49 +0900

    Remove: coaching model

--- coaching/models.py
+++ coaching/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 from register.models import *
-
-
-
-class Coaching:
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    client = models.ForeignKey(Dieter, on_delete=models.CASCADE)
-    client_name = models.CharField(max_length=10)
